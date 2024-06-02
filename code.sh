eba7ff995859dae85ac360c9624d4aeb775d18a5
xpynim <xpynim@gmail.com>
2024-06-02 18:45:50 +0700

    pyn1445dzq25:additems

---
+++ code.sh
@@ -0,0 +1,28 @@
+realtime_clock() {
+    clear
+
+    echo "
+    ▒██   ██▒ ██▓███ ▓██   ██▓ ███▄    █  ██▓ ███▄ ▄███▓
+    ▒▒ █ █ ▒░▓██░  ██▒██  ██▒ ██ ▀█   █ ▓██▒▓██▒▀█▀ ██▒
+    ░░  █   ░▓██░ ██▓▒▒██ ██░▓██  ▀█ ██▒▒██▒▓██    ▓██░
+     ░ █ █ ▒ ▒██▄█▓▒ ▒░ ▐██▓░▓██▒  ▐▌██▒░██░▒██    ▒██
+    ▒██▒ ▒██▒▒██▒ ░  ░░ ██▒▓░▒██░   ▓██░░██░▒██▒   ░██▒
+    ▒▒ ░ ░▓ ░▒▓▒░ ░  ░ ██▒▒▒ ░ ▒░   ▒ ▒ ░▓  ░ ▒░   ░  ░
+    ░░   ░▒ ░░▒ ░    ▓██ ░▒░ ░ ░░   ░ ▒░ ▒ ░░  ░      ░
+     ░    ░  ░░      ▒ ▒ ░░     ░   ░ ░  ▒ ░░      ░
+     ░    ░          ░ ░              ░  ░         ░
+                     ░ ░
+    "
+
+    trap 'clear; printf "\n"; return' SIGTSTP
+    while true; do
+		date_only=$(TZ=Asia/Tokyo date '+%d-%m-%Y')
+        date_oita=$(TZ=Asia/Tokyo date '+%H:%M:%S')
+        date_jakarta=$(TZ=Asia/Jakarta date '+%H:%M:%S')
+        combined_date="$date_only || Ōita-shi at $date_oita & Bandung at $date_jakarta"
+        printf "\r$combined_date |> press C-z to start"
+        sleep 1
+    done
+}
+
+realtime_clock
